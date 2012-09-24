7542b31f3c0f3c65441096406dcc5d7d058f7932
Bjarne Holen <bjarneholen@gmail.com>
2012-09-24 22:47:59 +0200

    made project compile :-)

--- src/com/github/bjarneh/main/TestStuff.java
+++ src/com/github/bjarneh/main/TestStuff.java
@@ -1,12 +1,12 @@
 package com.github.bjarneh.main;
 
-import com.github.bjarneh.handy.path.Walk;
+import com.github.bjarneh.utilz.handy;
 
 public class TestStuff{
 
     public static void main(String[] args) throws Exception {
         for(String s: args){
-            String[] files = Walk.walk(s);
+            String[] files = handy.pathWalk(s);
             for(String file: files){
                 System.out.printf("> %s\n", file);
             }
