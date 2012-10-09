67f7e1c6dc86117b8d00da2920a0ad4ceb91b4ac
Bjarne Holen <bjarneholen@gmail.com>
2012-10-10 01:30:39 +0200

    minor

--- src/com/github/bjarneh/start/Main.java
+++ src/com/github/bjarneh/start/Main.java
@@ -1,24 +1,9 @@
 package com.github.bjarneh.start;
 
-import java.io.ByteArrayInputStream;
-/// import java.io.FileOutputStream;
-
-import com.github.bjarneh.utilz.encoding;
 import com.github.bjarneh.utilz.io;
 
 class Main {
     public static void main(String[] args) throws Exception {
-///         io.pipe(System.in, System.out);
-
-        if(args.length < 1){
-            System.exit(1);
-        }
-
-        byte[] utf8bytes = io.raw(args[0]);
-        byte[] iso1bytes = encoding.fromTo(utf8bytes, "UTF-8", "ISO-8859-1");
-
-        ByteArrayInputStream bi = new ByteArrayInputStream(iso1bytes);
-        io.pipe(bi, System.out);
-
+        System.out.printf(" hello world\n");
     }
 }
